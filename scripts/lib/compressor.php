57c5a19b217a484b55c991889f2e6f79232127de
uhon <uhon@bird.(none)>
2012-04-14 23:59:09 +0200

    some small fixed to deployment

--- scripts/lib/compressor.php
+++ scripts/lib/compressor.php
@@ -51,6 +51,7 @@ foreach($scriptFiles as $file) {
     $scriptString .= "\n\n/* #### MERGED: " . $file . "*/\n" . file_get_contents($publicDir . "/" . $file);
 }
 
+throw new Exception($compressedDir);
 file_put_contents($compressedDir . "/traxpacking.css",  $styleString);
 file_put_contents($compressedDir . "/traxpacking.js",  $scriptString);
 
