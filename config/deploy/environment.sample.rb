4ee63ad9ac59a295016fef44cea7bffa0fb184cb
uhon <uhon@bird.(none)>
2012-04-17 21:01:35 +0200

    added some ignores for production-deployment

--- config/deploy/environment.sample.rb
+++ config/deploy/environment.sample.rb
@@ -4,10 +4,10 @@ set :user,        ""
 set :application, ""
 set :domain,      ""
 set :repository,  "git@github.com:websoft/traxpacking.git"
-set :deploy_to,   "/var/XXXX"
+set :deploy_to,   "/var/XYZ"
 set :shared_path, "#{deploy_to}/shared"
 set :remote_cached_repo,  "#{shared_path}/cached-copy"
-set :current_dir, "/../tp.whyjustify.com"
+set :current_dir, "/var/XYZ/www.xyz.com"
 set :use_sudo, false
 set :keep_releases, 5
 
